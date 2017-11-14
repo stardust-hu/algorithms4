f0c7bae537bce2ab7430cfad1ec6d5cce4138d58
stardust <yuhao.hu1992@gmail.com>
2017-11-14 23:00:25 +0800

    init

---
+++ src/Fundamentals_1/BasicProgModel_1_1/BinarySearch.java
@@ -0,0 +1,25 @@
+package Fundamentals_1.BasicProgModel_1_1;
+
+public class BinarySearch {
+
+    public static int rank(int key, int[] a){
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo<=hi){
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid])
+                hi = mid - 1;
+            else if (key > a[mid])
+                lo = mid + 1;
+            else
+                return mid;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5, 6};
+        System.out.printf(String.valueOf(rank(4, a)));
+    }
+}
